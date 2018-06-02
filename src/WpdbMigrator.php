014543db33a4dc1f2c5f9b99f504ed01f324a591
Miguel Muscat <miguelmuscat93@gmail.com>
2018-06-02 11:39:23 +0200

    Migrator now uses a more generic mechanism

--- src/WpdbMigrator.php
+++ src/WpdbMigrator.php
@@ -4,12 +4,16 @@ namespace RebelCode\EddBookings\Storage\Resource\Module;
 
 use Dhii\Exception\CreateInvalidArgumentExceptionCapableTrait;
 use Dhii\Exception\CreateOutOfRangeExceptionCapableTrait;
+use Dhii\Exception\CreateRuntimeExceptionCapableTrait;
 use Dhii\I18n\StringTranslatingTrait;
 use Dhii\Util\Normalization\NormalizeIntCapableTrait;
 use Dhii\Util\Normalization\NormalizeStringCapableTrait;
 use Dhii\Util\String\StringableInterface as Stringable;
+use Exception;
+use InvalidArgumentException;
 use RebelCode\Storage\Resource\WordPress\Wpdb\ExecuteWpdbQueryCapableTrait;
 use RebelCode\Storage\Resource\WordPress\Wpdb\WpdbAwareTrait;
+use RuntimeException;
 use wpdb;
 
 /**
@@ -38,6 +42,9 @@ class WpdbMigrator
     use CreateOutOfRangeExceptionCapableTrait;
 
     /* @since [*next-version*] */
+    use CreateRuntimeExceptionCapableTrait;
+
+    /* @since [*next-version*] */
     use StringTranslatingTrait;
 
     /**
@@ -64,17 +71,28 @@ class WpdbMigrator
     protected $migrationsDir;
 
     /**
+     * The current database version.
+     *
+     * @since [*next-version*]
+     *
+     * @var int|string|Stringable
+     */
+    protected $dbVersion;
+
+    /**
      * Constructor.
      *
      * @since [*next-version*]
      *
-     * @param wpdb              $wpdb          The WPDB instance.
-     * @param string|Stringable $migrationsDir The migrations directory path.
+     * @param wpdb                  $wpdb          The WPDB instance.
+     * @param string|Stringable     $migrationsDir The migrations directory path.
+     * @param int|string|Stringable $dbVersion     The current database version.
      */
-    public function __construct($wpdb, $migrationsDir)
+    public function __construct($wpdb, $migrationsDir, $dbVersion)
     {
         $this->_setWpdb($wpdb);
         $this->_setMigrationsDir($migrationsDir);
+        $this->_setDbVersion($dbVersion);
     }
 
     /**
@@ -102,32 +120,73 @@ class WpdbMigrator
     }
 
     /**
+     * Retrieves the current database version.
+     *
+     * @since [*next-version*]
+     *
+     * @return int|string|Stringable The current database version.
+     */
+    protected function _getDbVersion()
+    {
+        return $this->dbVersion;
+    }
+
+    /**
+     * Sets the current database version.
+     *
+     * @since [*next-version*]
+     *
+     * @param int|string |Stringable $dbVersion The current database version.
+     */
+    protected function _setDbVersion($dbVersion)
+    {
+        $this->dbVersion = $this->_normalizeInt($dbVersion);
+    }
+
+    /**
      * Performs database migration.
      *
      * @since [*next-version*]
      *
      * @param int|string|Stringable $target The migration target - can be a version, state, preset, etc.
+     *
+     * @throws InvalidArgumentException If the given target is invalid.
+     * @throws RuntimeException If failed to migrate to the given target.
      */
     public function migrate($target)
     {
-        $target = $this->_normalizeInt($target);
+        $target     = $this->_normalizeInt($target);
+        $current    = $this->_getDbVersion();
+        $difference = $target - $current;
 
-        switch ($target) {
-            case 0:
-                $targetDirPath = $this->_getMigrationsDir() . DIRECTORY_SEPARATOR . static::UP_MIGRATION_FILENAME;
-                break;
-
-            case 1:
-                $targetDirPath = $this->_getMigrationsDir() . DIRECTORY_SEPARATOR . static::DOWN_MIGRATION_FILENAME;
-                break;
-
-            default:
-                throw $this->_createOutOfRangeException(
-                    $this->__('Invalid migration target: "%s"', [$target]), null, null, $target
-                );
+        // No migration needed
+        if ($difference === 0) {
+            return;
         }
 
-        $this->_runMigrationFile($targetDirPath);
+        // Maximise the values to 1, since DB version 0 is not a real version and represents an uninitialized DB state
+        $current = max(1, $current);
+        $target  = max(1, $target);
+        // Get the list of migration versions to run
+        $migrations = range($current, $target);
+        // Determine the file names to look for, depending on migration direction
+        $filename = ($difference < 0)
+            ? static::DOWN_MIGRATION_FILENAME
+            : static::UP_MIGRATION_FILENAME;
+        // The root migrations directory
+        $directory = $this->_getMigrationsDir();
+
+        foreach ($migrations as $_version) {
+            $_path = implode(DIRECTORY_SEPARATOR, [$directory, $_version, $filename]);
+
+            try {
+                $this->_runMigrationFile($_path);
+            } catch (RuntimeException $exception) {
+                throw $this->_createRuntimeException(
+                    $this->__('Failed to migration to target "%s"', [$target]), null, $exception
+                );
+            }
+        }
     }
 
     /**
@@ -136,12 +195,18 @@ class WpdbMigrator
      * @since [*next-version*]
      *
      * @param string|Stringable $filePath The path to the migration file.
+     *
+     * @throws RuntimeException If failed to run the migration.
      */
     protected function _runMigrationFile($filePath)
     {
-        $sql = $this->_readSqlMigrationFile($filePath);
+        try {
+            $sql = $this->_readSqlMigrationFile($filePath);
 
-        $this->_getWpdb()->query($sql);
+            $this->_getWpdb()->query($sql);
+        } catch (Exception $exception) {
+            throw $this->_createRuntimeException($this->__('Failed to run migration"'), null, $exception);
+        }
     }
 
     /**
@@ -152,11 +217,17 @@ class WpdbMigrator
      * @param string|Stringable $filePath The path to the migration file.
      *
      * @return string|Stringable The read SQL.
+     *
+     * @throws RuntimeException If failed to read the migration file.
      */
     protected function _readSqlMigrationFile($filePath)
     {
         $filePath = $this->_normalizeString($filePath);
 
-        return file_get_contents($filePath);
+        if (is_file($filePath) && is_readable($filePath)) {
+            return file_get_contents($filePath);
+        }
+
+        throw $this->_createRuntimeException($this->__('Cannot read migration file "%s"', [$filePath]), null, null);
     }
 }
